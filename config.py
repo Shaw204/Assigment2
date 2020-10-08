d6c4ab994733f454b1dd624feb1c96acd73814b6
Shaw204 <1521382590zx@gmail.com>
2020-10-08 10:40:42 +0000

    Answer

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 
+# DO NOT EDIT
 
-# Replace this file!
+# Assignment for 18xz85
 
 from lib204 import wff
 P, Q, R, S, T = map(wff.Variable, 'PQRST')
-s1 = P
-s2 = Q
-s3 = R
-s4 = S
-s5 = T
-s6 = P
+s1 = (~P|(R|S))
+s2 = ((~R|S)&(~S|~R))
+s3 = (P|(~S|~R))
+s4 = ((P|~R)&(~R|~P))
+
+s5 = ((S>>P)>>((P&~S)|R))
+s6 = ((~P>>(~S&R))|~(S|~P))
--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-
 from lib204 import wff, semantic_interface
 
 try:
@@ -7,10 +6,10 @@ except:
     try:
         from .config import *
     except:
-        print("\n\tYou must download your config.py file first. See the assignment details in OnQ.\n")
+        print(
+            "\n\tYou must download your config.py file first. See the assignment details in OnQ.\n"
+        )
         exit(1)
-
-
 
 # First step: run this file (python run.py), and see the output.
 print('\nFormulae for Question 1:')
@@ -30,7 +29,6 @@ print(" s6: %s" % s6)
 #   (b) determine which ones are equivalent (i.e., have the same truth table),
 #   (c) find a model satisfied by one formula, but none of the others
 
-
 # (a) Put the truth tables (text file, written on paper + picture, whatever)
 #     into the Q1a folder
 
@@ -38,22 +36,18 @@ print(" s6: %s" % s6)
 #     theories. These are two wrong answers, but they give you an idea of how
 #     to write your response.
 
-q1b = [ [s1,s2,s3,s4] ] # they are all equivalent.
+#q1b = [ [s1,s2,s3,s4] ] # they are all equivalent.
 #q1b = [ [s1], [s2], [s3], [s4] ] # they are all different
-#q1b = ??? # the right answer please!
+q1b = [[s1], [s3], [s2, s4]]  # the right answer please!
 
 # (c) Variable q1c should be a dictionary mapping variables to True or False.
 #     Use only what you need of P, Q, R, S, T. An example (almost certainly
 #     incorrect) answer is given as an example.
 q1c = {
-    P: True,
-    Q: False,
+    P: False,
     R: True,
-    S: False,
-    T: True
+    S: True,
 }
-
-
 
 ################################################################################
 # Question 2
@@ -68,8 +62,8 @@ print("s5 = %s" % s5.dump('python'))
 print("s6 = %s" % s6.dump('python'))
 
 # replace the following two lines with what the above code prints
-s5 = ~(P | Q)
-s6 = (P & Q) | R
+s5 = ((S >> P) >> ((P & ~S) | R))
+s6 = ((~P >> (~S & R)) | ~(S | ~P))
 
 # (a) Put the parse trees inside folder Q2a. You can do it on paper and take a
 #     photo, or use drawing software. This will not be marked unless requested,
@@ -86,31 +80,40 @@ s6 = (P & Q) | R
 #      - double negation
 #      - etc.
 
-s5nnf = [
-    [~(P | Q), 'starting formula'],
-    [~P & ~Q, 'de Morgans']
-]
+s5nnf = [[((S >> P) >> ((P & ~S) | R)), 'starting formula'],
+         [(~S | P) >> ((P & ~S) | R), 'replace implications'],
+         [~(~S | P) | ((P & ~S) | R), 'replace implications'],
+         [(~~S & ~P) | ((P & ~S) | R), 'de Morgans'],
+         [(S & ~P) | ((P & ~S) | R),
+          'double negation -- in negation normal form']]
 
-s6nnf = [
-    [(P & Q) | R, 'starting formula -- already in negation normal form']
-]
-
+s6nnf = [[((~P >> (~S & R)) | ~(S | ~P)), 'starting formula'],
+         [((~~P | (~S & R)) | ~(S | ~P)), 'replace implications'],
+         [((P | (~S & R)) | ~(S | ~P)), 'double negation'],
+         [((P | (~S & R)) | (~S & ~~P)), 'de Morgans'],
+         [((P | (~S & R)) | (~S & P)),
+          'double negation -- in negation normal form']]
 
 # (c) Copy s5 and s6 formulae into the first steps below, and show the steps to
 #     convert to CNF using distribution, de Morgan's, implication removal, etc.
 #     An example is given, but it is for the wrong formula. You /must/ provide
 #     an explanation for each step. Possible explanations are listed above.
 
-s5cnf = [
-    [~(P | Q), 'starting formula'],
-    [~P & ~Q, 'de Morgans']
-]
+s5cnf = [[((S >> P) >> ((P & ~S) | R)), 'starting formula'],
+         [((~S | P) >> ((P & ~S) | R)), 'replace implications'],
+         [~(~S | P) | ((P & ~S) | R), 'replace implications'],
+         [(~~S & ~P) | ((P & ~S) | R), 'de Morgans'],
+         [(S & ~P) | ((P & ~S) | R), 'double negation'],
+         [(S & ~P) | (P | R) & (~S | R), 'distribution'],
+         [(P | R | S) & (~S | R | ~P), 'distribution -- in CNF']]
 
-s6cnf = [
-    [(P & Q) | R, 'starting formula'],
-    [(P | R) & (Q | R), 'distribution']
-]
-
+s6cnf = [[((~P >> (~S & R)) | ~(S | ~P)), 'starting formula'],
+         [((~~P | (~S & R)) | ~(S | ~P)), 'replace implications'],
+         [((P | (~S & R)) | ~(S | ~P)), 'double negation'],
+         [((P | (~S & R)) | (~S & ~~P)), 'de Morgans'],
+         [((P | (~S & R)) | (~S & P)), 'double negation'],
+         [(P | ~S) & (P | R) | (~S & P), 'distribution'],
+         [(~S | P) & (R | P), 'distribution -- in CNF']]
 
 # (d) Build the Tseitin encoding of both s5 and s6 using the semantic_interface
 #     library to create auxiliary variables as necessary. Examples for different
@@ -118,20 +121,27 @@ s6cnf = [
 #     and you may re-use auxiliary variables as necessary.
 
 # e.g., s5 = ~(P | Q)
+s5 = (P | R | S) & (~S | R | ~P)
 s5tseitin = semantic_interface.Encoding()
 # first argument is the formula; second is the variable name.
-x1 = s5tseitin.tseitin(P | Q, 'x1')
-x2 = s5tseitin.tseitin(~x1, 'x2')
+x1 = s5tseitin.tseitin(~S, 'x1')
+x2 = s5tseitin.tseitin(~P, 'x2')
+x3 = s5tseitin.tseitin((x1 | x2), 'x3')
+x4 = s5tseitin.tseitin(x3 | R, 'x4')
+x5 = s5tseitin.tseitin((P | R), 'x5')
+x6 = s5tseitin.tseitin((x5 | S), 'x6')
+x7 = s5tseitin.tseitin(x4 & x6, 'x7')
 # This final step is required -- use your last variable, corresponding to the top
 #  of the parse tree, to finalize your Tseitin encoding.
-s5tseitin.finalize(x2)
+s5tseitin.finalize(x7)
 
 # e.g., s6 = (P & Q) | R
+s6 = (~S | P) & (R | P)
 s6tseitin = semantic_interface.Encoding()
-x1 = s6tseitin.tseitin(P & Q, 'x1')
-x2 = s6tseitin.tseitin(x1 | R, 'x2')
-s6tseitin.finalize(x2)
-
-
+x1 = s6tseitin.tseitin(~S, 'x1')
+x2 = s6tseitin.tseitin(x1 | P, 'x2')
+x3 = s6tseitin.tseitin(R | P, 'x3')
+x4 = s6tseitin.tseitin(x2 & x3, 'x4')
+s6tseitin.finalize(x4)
 
 print()
